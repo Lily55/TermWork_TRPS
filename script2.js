c5379e850f67e7ccba1963a2915b20563d40f8db
Masha <scarmasha@gmail.com>
2023-11-02 23:17:24 +0300

    Works without push

--- script2.js
+++ script2.js
@@ -1,4 +1,4 @@
-const operators = ["*", "+", "XOR", "<->", "|", "->", "NOT"];
+const operators = ["*", "+", "XOR", "<->", "|", "->"]; // добавить NOT обратно
 const variables = ["A", "B", "C", "D"];
 const symbols = operators + variables;
 
@@ -16,35 +16,12 @@ let inputStack = ["A", "B", "C", "*", "+", "D", "XOR"];
 class BooleanTree{
     constructor(){
         this.root = null;
+        this.operations = null;
     }
 
-    // checkOperator(value){
-    //     for(let i = 0; i < operators.length; i++)
-    //     if (value == operators[i]) return true;
-    // }
-
-
-    // createTree(inputLine){
-    //     let stack = [];
-
-    //     for(let i = 0; i < inputLine.length; i++)
-    //     {
-    //         if(!this.checkOperator(inputLine[i]))
-    //         {
-    //             let newLeaf = new Node(inputLine[i]);
-    //             stack.push(newLeaf);
-    //         }
-    //         else{
-    //             let newRoot = new Node(inputLine[i]);
-    //             newRoot.right = stack.pop();
-    //             newRoot.left = stack.pop();
-    //             stack.push(newRoot);
-    //             this.root = newRoot;
-    //         }
-    //     }
-    // }
 
     createTree(numOperators){
+        this.operations = numOperators;
         let queue = [];
         let newNode = new Node(operators[Math.floor(Math.random() * operators.length)]);
         numOperators--;
@@ -72,10 +49,56 @@ class BooleanTree{
             }
         }
 
-        for(let i = 0; i< 3; i++)
+        for(let i = 0; i < queue.length; i++)
         {
             queue[i].value = variables[Math.floor(Math.random()*variables.length)];
         }
+    }
+
+    setBrackets(currentString,currentNode){
+        let openBracket = new Node("(");
+        let closeBracket = new Node(")");
+        let currentIndex = currentString.indexOf(currentNode)
+
+        if(operators.includes(currentNode.value))
+        {
+            currentString.splice(currentIndex,0,openBracket);
+            currentIndex++;
+            currentString.splice(currentIndex+1,0,closeBracket);
+        }
+
+        if(currentNode.left != null && currentNode.right != null)
+        {
+            currentString.splice(currentIndex,0,currentNode.left);
+            currentIndex++;
+            currentString.splice(currentIndex+1,0,currentNode.right);
+            this.setBrackets(currentString,currentNode.left);
+            this.setBrackets(currentString,currentNode.right);
+        }
+
+        return currentString;
+    }
+
+    getString(){
+        if(this.root === null) return false
+        let currentString = [];
+
+        let finalString = []
+        let currentNode = this.root;
+
+        currentString.push(currentNode)
+        currentString.unshift(currentNode.left);
+        currentString.push(currentNode.right);
+
+        currentString = this.setBrackets(currentString,currentNode.left);
+        currentString = this.setBrackets(currentString,currentNode.right);
+
+        console.log(currentString);
+
+        for(let i = 0; i < currentString.length; i++)
+            finalString[i] = currentString[i].value;
+
+        return finalString.join(' ');
     }
 
 }
@@ -83,4 +106,5 @@ class BooleanTree{
 const myTree = new BooleanTree();
 myTree.createTree(7);
 console.log(myTree);
+console.log(myTree.getString())
 
